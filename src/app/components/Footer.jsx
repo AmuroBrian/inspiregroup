d52a41f1a86fd9f91b698ea4215d35ca860170a2
denseeen <villanueva.194566@cubao.sti.edu.ph>
2025-02-17 17:19:09 +0800

    footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={{backgroundColor: '#fff', color: '#282c34', textAlign: 'center', padding: '20px 0'}}>
+      <p>Copyright © 2024 Inspire Holdings Inc. - All Rights Reserved.</p>
+      <div style={{marginBottom: '10px'}}>
+        
+      </div>
+      <p>Powered by</p>
+      <p>Inspire Group</p>
+    </footer>
+  )
+}
+
+export default Footer
+
