6c0366d37e23a0cf0961b3b78597ef19476d0be9
Amuro Brian <perezbrian.sti@gmail.com>
2025-02-22 18:08:28 +0800

    Fixed changes in geoblocker

--- src/app/not-legal.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotLegal() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>Access Denied</h1>
-      <p>This website is not accessible in your country.</p>
-    </div>
-  );
-}
