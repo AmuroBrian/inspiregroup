3842b9ca96a8b0b57a2c6c7872c00cb3fdd6f4de
Amuro Brian <perezbrian.sti@gmail.com>
2025-02-23 12:05:41 +0800

    Added Picture and Changed

--- src/app/components/WelcomeToHol.jsx
+++ src/app/components/WelcomeToHol.jsx
@@ -4,13 +4,13 @@ import React from "react";
 
 export default function WelcomeToHol() {
   return (
-    <div className="flex items-center justify-center min-h-[70vh] bg-white px-4">
+    <div className="flex items-center justify-center min-h-[70vh] bg-white px-4 flex-col">
       {/* Grid Container */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-6xl w-full">
         {/* Company Info Section */}
         <div className="flex flex-col justify-center text-black md:order-1">
           <h2 className="text-2xl md:text-3xl font-bold mb-4 mt-4 text-center md:text-left">
-            Welcome to Inspire Group
+            Welcome to Inspire Holdings Group
           </h2>
           <p className="text-md md:text-lg leading-relaxed text-center md:text-left">
             At Inspire Group, we are dedicated to providing innovative solutions
@@ -31,6 +31,21 @@ export default function WelcomeToHol() {
             className="w-full h-auto rounded-lg shadow-lg"
           />
         </div>
+      </div>
+      <div className="text-center p-7 text-xl">
+        <span className="font-bold">
+          For Foreign Entrepreneurs and Investors
+        </span>{" "}
+        – A World-Class Private Banking Experience At Inspire Group, we provide
+        an exclusive network of elite private bankers dedicated to serving
+        global entrepreneurs and investors. With strong connections to
+        government authorities, major conglomerates, and key administrative
+        bodies, we continue to grow as one of the most influential trading firms
+        in the Philippines. Our unwavering trust and proven track record enable
+        us to achieve what others deem impossible, offering exclusive business
+        opportunities that go beyond conventional boundaries. At Inspire, we
+        don’t just navigate the future—we transform your vision into a lasting
+        legacy of success.
       </div>
     </div>
   );
